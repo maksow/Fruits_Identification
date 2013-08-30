5e8c0ae7409c12c556e224ad2304088b700c8cc6
Sowjanya <soujanyamulkala@gmail.com>
2013-08-30 10:14:24 +0530

    Revert "project"
    This reverts commit 85a8b9d6e9946cff3607700da3b5056ea7323e53.

--- kmeans/irisfuzzy.m
+++
@@ -1,6 +0,0 @@
-load fisheriris
-       data = meas(:,1:4);
-       testdata=data(1:50,:);
-       traindata=data(51:150,:);
-       csvwrite('testx.dat',testdata);
-       csvwrite('trainx.dat',traindata);
